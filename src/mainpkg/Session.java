6b05292be38a2f095453ea048be9c767cc0847ce
Mary <mmelni4@uic.edu>
2017-11-20 23:11:36 -0600

    test

--- src/mainpkg/Session.java
+++ src/mainpkg/Session.java
@@ -1,12 +1,10 @@
 package mainpkg;
 import Securitypkg.*;
-import UI.*;
 
 public class Session {
 
 	public static void main(String[] args)
 	{
-		Chat c = new Chat();
 		System.out.println("Testing standard out");
 		RSA sec = new RSA(149,157);
 	}
