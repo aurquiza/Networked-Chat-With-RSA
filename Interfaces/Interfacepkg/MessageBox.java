3bd5723d7b54d297041b745a44f79d9b790a3f67
Alexis <aurqui7@uic.edu>
2017-12-07 19:38:22 -0600

    comments for messageBox class

--- Interfaces/Interfacepkg/MessageBox.java
+++ Interfaces/Interfacepkg/MessageBox.java
@@ -14,17 +14,22 @@ import javax.swing.JTextArea;
 @SuppressWarnings("serial")
 public class MessageBox extends JPanel
 {
-
+	// declared static variables that will have previous messages show for user
 	static JTextArea messageHistory;
 	
+	// constructor
 	public MessageBox()
 	{
+		// create it for jpanel
 		messageHistory = new JTextArea(20, 30);
 		messageHistory.setEditable(false);
+		// add to jpanel and change color of the background
 		this.setBackground(new Color(204, 255, 245));
 		this.add(new JScrollPane(messageHistory));
 	}
 	
+	// this static method appends a message to the message box based on the
+	// parameter passed in
 	public static void addMessage(String msg)
 	{
 		messageHistory.append(msg + "\n");
