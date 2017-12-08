c7adb46370273e82a0c9359bfc3780cfce08acde
Alexis <aurqui7@uic.edu>
2017-12-07 19:56:11 -0600

    mary's comments

--- Interfaces/Interfacepkg/ClientList.java
+++ Interfaces/Interfacepkg/ClientList.java
@@ -9,11 +9,9 @@ import javax.swing.BorderFactory;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
 import javax.swing.ListSelectionModel;
 import javax.swing.ScrollPaneLayout;
 
-import Networkpkg.NameAndKeyPair;
 
 /* 
  * Client list has an updated list of all
@@ -25,48 +23,51 @@ public class ClientList {
 	
 	private static ClientList clientlist = null;
 	private static JPanel clientPanel = null;
-	private JList<String> list;
+	private JList<String> list; // allows select option
 	private static Vector<String> currentList = new Vector<String>();
+	//thread safe container
 	
 	private ClientList(){
 		clientPanel = new JPanel();
 		list = new JList<String>();
-		list.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
-
-		list.setVisibleRowCount(23);
-		
+		list.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);		
 		JScrollPane panel = new JScrollPane(list);
-		panel.setLayout(new ScrollPaneLayout());
+		panel.setLayout(new ScrollPaneLayout()); //in case client list overlaps panel size
 		panel.setBorder(BorderFactory.createTitledBorder("Clients Online:"));
 		panel.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
 		panel.setPreferredSize(new Dimension(280, 390));
 		clientPanel.add(panel, BorderLayout.CENTER);
 	}
-	
+
+	// receives call from Chat to clear clientList instance
+	// when user is trying to reenter chat in the same session
 	public void emptyClientList()
 	{
 		currentList.clear();
 		list.setListData(currentList);
 	}
 	
+	// update list in GUI whenever new user is joined
 	public void addNewClient(String user)
 	{
 		currentList.addElement(user);
 		list.setListData(currentList);
-		//list.append(name + "\n");
 	}
 	
+	//update clientList when user is disconnected from chat
 	public void removeClient(String user)
 	{
 		currentList.remove(user);
 		list.setListData(currentList);
 	}
 	
+	// create a vector of clients selected for sending message to
 	public List<String> getChosenClients()
 	{
 		return list.getSelectedValuesList();
 	}
 	
+	//singleton design for constructor, used for GUI right side panel 
 	public static JPanel getClientList()
 	{
 
@@ -77,6 +78,7 @@ public class ClientList {
 	      return clientPanel;
 	}
 	
+	//used client list modification
 	public static ClientList getClientBox()
 	{
 		if(clientlist == null)
